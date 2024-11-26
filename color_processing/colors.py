c8cb0fab4a739c8ae434b814d51a6b6417e78523
cop <32881179+cop-discord@users.noreply.github.com>
2024-11-25 16:55:38 -0800

    feat(colors): added the search function for searching color names

--- color_processing/colors.py
+++ color_processing/colors.py
@@ -1,87 +1,93 @@
+import webcolors
+import asyncio
+import re
+import numpy as np
+import math
+import pickle
+import aiohttp
+
 from typing import Callable, Optional, List, Tuple, Any, Union, Dict
 from tools import thread
 from discord.ext.commands import Context, CommandError, ColorConverter, ColourConverter, Converter
 from fast_string_match import closest_match_distance as cmd
 from discord.ext import commands
 from discord import Color, User, Member, File, Embed, Message
-import pickle
-import aiohttp
-import seaborn as sns
 from loguru import logger
-import numpy as np
-import math
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup
 from pkg_resources import resource_filename
-import asyncio
-import re
 from io import BytesIO
 from itertools import chain
-import webcolors
-from pydantic import BaseModel as BM
-
-class BaseModel(BM):
-    class Config:
-        arbitrary_types_allowed=True
-class WebSafe(BaseModel):
-    hex: str
-    rgb: list
-
-class ColorInfoResponse(BaseModel):
-    name: Optional[str] = None
-    hex: str
-    websafe: WebSafe
-    rgb: list
-    brightness: int
-    shades: List[str]
-    palette: BytesIO
-    image: BytesIO
-
-    @property
-    def embed(self) -> Dict[str, Any]:
-        shade = ", ".join(m.strip("#") for m in self.shades[:4])
-        rgb = f"({self.rgb[0]}, {self.rgb[1]}, {self.rgb[2]})"
-        embed = Embed(title=f"{hex[0]} ({hex[1]})", color=Color.from_str(hex[1]))
-        embed.add_field(name="Websafe", value=f"`{self.websafe.hex} ({self.websafe.rgb})`", inline=True)
-        embed.add_field(name="RGB", value=f"`{rgb}`", inline=True)
-        embed.set_image(url="attachment://palette2.png")
-        embed.add_field(name="Brightness", value=self.brightness, inline=True)
-        embed.add_field(name="Shades", value=f"```{shade}```", inline=False)
-        embed.set_thumbnail(url="attachment://palette.png")
-        return {"files": [File(fp = self.image, filename = "color.png"), File(fp = self.palette, filename = "palette.png")], "embed": embed}
-    
-    async def to_message(self, ctx: Context) -> Message:
-        return await ctx.send(**self.embed)
-    
+from lxml import html
+from .models import SearchResponse, ColorInfoResponse, SearchResult
 
 
-
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
 
 class Colors:
     def __init__(self, thread_handler: Optional[Callable] = thread):
         self.thread_handler = thread_handler
         self.colors = None
 
-    async def generate_palette(self, hex_color: str):
+    async def scrape_name(self, color: str) -> Optional[str]:
+        async with ClientSession() as session:
+            async with session.get(f"https://www.color-name.com/hex/{color.lstrip('#')}", headers=HEADERS) as response:
+                if response.status != 200:
+                    return None
+                data = BeautifulSoup(await response.text(), "html.parser")
+                s = data.find("p", attrs={"class": "hex-rgb-info"})
+                
+                if not s:
+                    return None
+
+                name = s.get_text().split("RGB")[0].split(":")[1].strip()
+
+                await self.dump(color, name)
+                return name
+
+    async def dump(self, color: str, name: str):
         @self.thread_handler
-        def generate(hex_color: str):
+        def execute(color: str, name: str):
+            pickle_file_path = resource_filename(__name__, 'data/colors.pkl')
+            if not color.startswith("#"):
+                color = f"#{color}"
+            with open(pickle_file_path, "rb") as read_file:
+                colors = pickle.loads(read_file.read())
+            
+            with open(pickle_file_path, "wb") as file:
+                colors = list(colors)
+                colors.append((name, color,))
+                colors = tuple(colors)
+                file.write(pickle.dumps(colors))
+        return await execute(color, name)
+
+
+    async def generate_palette(self, hex__color: str):
+        @self.thread_handler
+        def generate(hex__color: str):
             from PIL import Image
             from io import BytesIO
             output = BytesIO()
-            image = Image.new("RGB", (150, 150), hex_color)
+            image = Image.new("RGB", (150, 150), hex__color)
             image.save(output, format="PNG")
             output.seek(0)
             return output
-        return await generate(hex_color)
+        return await generate(hex__color)
 
     def brightness(self, color_rgb):
         return (0.299 * color_rgb[0] + 0.587 * color_rgb[1] + 0.114 * color_rgb[2]) / 255
 
-    def generate_color_swatch(self, color_hex):
-        color_rgb = tuple(int(color_hex.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
+    def generate_color_swatch(self, color_hex_):
+        color_rgb = tuple(int(color_hex_.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
         color_swatch = np.full((100, 100, 3), color_rgb, dtype=np.uint8)
         return color_swatch
 
-    def get_shades(self, color_hex: str, amount: Optional[int] = 5, darker: Optional[bool] = True) -> List[str]:
-        rgb_color = webcolors.hex_to_rgb(color_hex)
+    def get_shades(self, color_hex_: str, amount: Optional[int] = 5, darker: Optional[bool] = True) -> List[str]:
+        rgb_color = webcolors.hex_to_rgb(color_hex_)
         amount = amount - 1
         if darker:
             shades = [
@@ -105,7 +111,7 @@ class Colors:
                 )
                 for i in range(1, amount + 1)
             ]
-        shades.insert(0, color_hex)
+        shades.insert(0, color_hex_)
         return shades
 
     async def generate_multi_palette(self, shades: List[str]):
@@ -119,8 +125,8 @@ class Colors:
 
             num_per_row = (num_colors + 1) // 2  # Number of images per row
 
-            for color_hex in shades:
-                color_rgb = tuple(int(color_hex.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
+            for color_hex_ in shades:
+                color_rgb = tuple(int(color_hex_.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
                 color_swatch = np.full((100, 100, 3), color_rgb, dtype=np.uint8)
                 images.append(color_swatch)
 
@@ -136,16 +142,16 @@ class Colors:
             font = ImageFont.load_default()
             draw = ImageDraw.Draw(collage_image)
 
-            for i, color_hex in enumerate(shades):
+            for i, color_hex_ in enumerate(shades):
                 row_index = i // num_per_row
                 col_index = i % num_per_row
                 x = col_index * 100 + 10
                 y = row_index * 100 + 70
 
-                color_rgb = tuple(int(color_hex.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
+                color_rgb = tuple(int(color_hex_.lstrip("#")[i: i + 2], 16) for i in (0, 2, 4))
                 label_color = "black" if self.brightness(color_rgb) > 0.5 else "white"
 
-                draw.text((x, y), color_hex, fill=label_color, font=font)
+                draw.text((x, y), color_hex_, fill=label_color, font=font)
 
             buf = BytesIO()
             collage_image.save(buf, format="PNG")
@@ -153,8 +159,8 @@ class Colors:
             return buf
         return await generate(shades)
 
-    def hex_to_brightness(self, color_hex: str) -> int:
-        color_rgb = tuple(int(color_hex.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
+    def hex_to_brightness(self, color_hex_: str) -> int:
+        color_rgb = tuple(int(color_hex_.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
         return int(self.brightness(color_rgb) * 100.0)
 
     async def load_color_map(self):
@@ -178,9 +184,9 @@ class Colors:
             return [m for m in colors if m[0] == match]
         return None
 
-    def hex_to_rgb(self, hex_color: Any):
-        hex_color = hex_color.lstrip("#")
-        return tuple(int(hex_color[i: i + 2], 16) for i in (0, 2, 4))
+    def hex_to_rgb(self, hex__color: Any):
+        hex__color = hex__color.lstrip("#")
+        return tuple(int(hex__color[i: i + 2], 16) for i in (0, 2, 4))
 
     def rgb_distance(self, color1: Any, color2: Any):
         r1, g1, b1 = color1
@@ -201,45 +207,75 @@ class Colors:
 
         return closest_color
 
-    async def find_name(self, hex_: str):
-        async def _find_name(hex_: str, colors: tuple):
+    async def find_name(self, hex__: str):
+        async def _find_name(hex__: str, colors: tuple):
             try:
-                return [c for c in colors if c[1] == hex_][0]
+                return [c for c in colors if c[1] == hex__][0]
             except Exception:
                 return None
 
-        data = await asyncio.gather(*[_find_name(hex_, c) for c in self.colors])
+        data = await asyncio.gather(*[_find_name(hex__, c) for c in self.colors])
         data = [d for d in data if d is not None]
         if len(data) != 0:
             return data[0]
         else:
             return "unnamed"
 
-    def get_websafe(self, color_hex: str) -> tuple:
-        print(color_hex)
-        rgb = webcolors.hex_to_rgb(color_hex)
+    def get_websafe(self, color_hex_: str) -> tuple:
+        print(color_hex_)
+        rgb = webcolors.hex_to_rgb(color_hex_)
         web_safe_rgb = [int(round(val / 51) * 51) for val in rgb]
         print(web_safe_rgb)
-        web_safe_hex = webcolors.rgb_to_hex(web_safe_rgb)
-        return web_safe_hex, web_safe_rgb
+        web_safe_hex_ = webcolors.rgb_to_hex(web_safe_rgb)
+        return web_safe_hex_, web_safe_rgb
 
-    async def closest_color(self, color_hex: str, name: Optional[bool] = False, with_websafe: Optional[bool] = False):
+    async def closest_color(self, color_hex_: str, name: Optional[bool] = False, with_websafe: Optional[bool] = False):
         await self.load_color_map()
         color_list = []
         for colo in self.colors:
             _color_list = [c[1] for c in colo]
             color_list.extend(_color_list)
-        nearest = self.nearest_color(color_hex, color_list)
+        nearest = self.nearest_color(color_hex_, color_list)
         next((c for colo in self.colors for c in colo if c[1] == nearest), None)
-        rgb = webcolors.hex_to_rgb(color_hex)
+        rgb = webcolors.hex_to_rgb(color_hex_)
         web_safe_rgb = [round(val / 51) * 51 for val in rgb]
-        web_safe_hex = webcolors.rgb_to_hex(web_safe_rgb)
+        web_safe_hex_ = webcolors.rgb_to_hex(web_safe_rgb)
         if name:
-            color_name = await self.find_name(web_safe_hex)
+            color_name = await self.find_name(web_safe_hex_)
             data = (color_name, nearest)
         else:
             data = nearest
         return data
+    
+    async def search(self, query: str) -> Optional[SearchResponse]:
+        async with ClientSession() as session:
+            async with session.get(f"https://www.color-name.com/search/{query}", headers = HEADERS) as response:
+                data = await response.text()
+        tree = html.fromstring(data)
+        results = []
+        uls = tree.xpath("/html/body/div[2]/ul/li")
+        if not uls:
+            return None
+        for ul in uls:
+            bg = uls[0].xpath("./a/svg/@style")[0]
+            name = uls[0].xpath("./h3/a/text()")[0]
+            results.append({"name": name, "hex_": f'#{bg.split("#", 1)[1].split(";", 1)[0]}'})
+        return SearchResponse(results)
+    
+    async def detail(self, result: SearchResult) -> ColorInfoResponse:
+        websafe, websafe_rgb = self.get_websafe(result.hex_)
+        palette = await self.generate_palette(result.hex_)
+        rg = webcolors.hex_to_rgb(result.hex_)
+        _shades = self.get_shades(result.hex_, 11)
+        _shades.extend(self.get_shades(result.hex_, 11, False))
+        shades = []
+        for s in _shades:
+            if s not in shades:
+                shades.append(s)
+        palette2 = await self.generate_multi_palette(shades)
+        rgb = f"({rg[0]}, {rg[1]}, {rg[2]})"
+        data = {"name": result.name, "hex_": result.hex_, "websafe": {"hex_": websafe, "rgb": websafe_rgb}, "rgb": (rg[0], rg[1], rg[2]), "brightness": self.hex_to_brightness(result.hex_), "shades": shades, "image": palette, "palette": palette2}
+        return ColorInfoResponse(**data)
     
     async def color_info(self, query: Union[str, Member, User]) -> ColorInfoResponse:
         if isinstance(query, str):
@@ -249,63 +285,34 @@ class Colors:
             query = await self.get_dominant_color(query.display_avatar.url)
         await self.load_color_map()
         if query.startswith("#"):
-            hex = ((await self.closest_color(query, True))[0][0], query)
+            hex_ = ((await self.closest_color(query, True))[0][0], query)
         else:
             try:
                 Color.from_str(f"#{query}")
             except Exception:
                 pass
-            hex = await self.color_search(query)
-        websafe, websafe_rgb = self.get_websafe(hex[1])
-        palette = await self.generate_palette(hex[1])
-        rg = webcolors.hex_to_rgb(hex[1])
-        shades = self.get_shades(hex[1], 11)
-        shades.extend(self.get_shades(hex[1], 11, False))
+            hex_ = await self.color_search(query)
+        websafe, websafe_rgb = self.get_websafe(hex_[1])
+        palette = await self.generate_palette(hex_[1])
+        rg = webcolors.hex_to_rgb(hex_[1])
+        _shades = self.get_shades(hex_[1], 11)
+        _shades.extend(self.get_shades(hex_[1], 11, False))
+        shades = []
+        for s in _shades:
+            if s not in shades:
+                shades.append(s)
         palette2 = await self.generate_multi_palette(shades)
         rgb = f"({rg[0]}, {rg[1]}, {rg[2]})"
-        data = {"name": hex[0], "hex": hex[1], "websafe": {"hex": websafe, "rgb": websafe_rgb}, "rgb": (rg[0], rg[1], rg[2]), "brightness": self.hex_to_brightness(hex[1]), "shades": shades, "palette": palette, "image": palette2}
+        data = {"name": hex_[0], "hex_": hex_[1], "websafe": {"hex_": websafe, "rgb": websafe_rgb}, "rgb": (rg[0], rg[1], rg[2]), "brightness": self.hex_to_brightness(hex_[1]), "shades": shades, "image": palette, "palette": palette2}
+        if data["name"] in (None, "u"):
+            data["name"] = await self.scrape_name(data["hex_"])
         return ColorInfoResponse(**data)
-
-    async def color_info_embed(self, ctx: Context, query: str):
-        if query.startswith("https://"):
-            query = await self.get_dominant_color(query)
-        await self.load_color_map()
-        if query.startswith("#"):
-            hex = ((await self.closest_color(query, True))[0][0], query)
-        else:
-            try:
-                Color.from_str(f"#{query}")
-            except Exception:
-                pass
-            hex = await self.color_search(query)
-        websafe = self.get_websafe(hex[1])
-        palette = await self.generate_palette(hex[1])
-        rg = webcolors.hex_to_rgb(hex[1])
-        shades = self.get_shades(hex[1], 11)
-        shades.extend(self.get_shades(hex[1], 11, False))
-        palette2 = await self.generate_multi_palette(shades)
-        shade = ", ".join(m.strip("#") for m in shades[:4])
-        rgb = f"({rg[0]}, {rg[1]}, {rg[2]})"
-        embed = Embed(title=f"{hex[0]} ({hex[1]})", color=Color.from_str(hex[1]))
-        embed.add_field(name="Websafe", value=f"`{websafe}`", inline=True)
-        embed.add_field(name="RGB", value=f"`{rgb}`", inline=True)
-        embed.set_image(url="attachment://palette2.png")
-        embed.add_field(name="Brightness", value=self.hex_to_brightness(hex[1]), inline=True)
-        embed.add_field(name="Shades", value=f"```{shade}```", inline=False)
-        embed.set_thumbnail(url="attachment://palette.png")
-        return await ctx.send(
-            files=[
-                File(fp=palette2, filename="palette2.png"),
-                File(fp=palette, filename="palette.png"),
-            ],
-            embed=embed,
-        )
 
     async def color_search(self, query: str, with_websafe: Optional[bool] = False):
         await self.load_color_map()
         if query == "black":
             return ("Black", "#010101")
-        if hex_match := re.match(r"#?[a-f0-9]{6}", query.lower()):
+        if hex__match := re.match(r"#?[a-f0-9]{6}", query.lower()):
             color_name = await self.closest_color(query, True, True)
             return (color_name[1], query)
         matches = []
@@ -369,7 +376,7 @@ class ColorHolder:
                         return Color.from_str(_)
                 except Exception as e:
                     logger.info(f"Color Converter Errored with : {e}")
-                    raise CommandError("Invalid color hex given")
+                    raise CommandError("Invalid color hex_ given")
             
 class ColorInfo(Converter):
     async def convert(self, ctx: Context, argument: Union[Color, str, Member, User]):
@@ -382,3 +389,4 @@ class ColorInfo(Converter):
 
 commands.ColorInfo = ColorInfo
 ColourConverter.convert = ColorConverter.convert
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='color_processing',
-    version='0.5',  # Consider using '0.1.0' for semantic versioning
+    version='0.7',  # Consider using '0.1.0' for semantic versioning
     packages=find_packages(),
     include_package_data=True,
     package_data={
